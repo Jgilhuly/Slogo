ece364a9d31fc30705471d83bf548f3d0b2312d8
gtnet8@gmail.com <gtnet8@gmail.com>
2015-02-25 15:53:07 -0500

    implemented boolean commands

--- src/command/bool/NotEqualCommand.java
+++ src/command/bool/NotEqualCommand.java
@@ -1,7 +1,17 @@
 package command.bool;
 
+import java.util.List;
+
 import command.Command;
 
-public class NotEqualCommand{
+public class NotEqualCommand extends Command {
+
+	@Override
+	//returns 1 if the value of expr1 and the value of expr2 are not equal, otherwise 0
+	public double calculateValue(List<Object> param) {
+		double expr1 = (double) param.get(0);
+		double expr2 = (double) param.get(1);
+		return (expr1 != expr2) ? 1: 0 ; //condition ? value_if_true : value_if_false
+	}
 
 }
