c7522248b6b3eca2071185675fddf439ac66db2d
keikumata <ky52@duke.edu>
2015-02-26 13:11:34 -0500

    changed power and towards

--- src/command/math/PowerCommand.java
+++ src/command/math/PowerCommand.java
@@ -5,7 +5,7 @@ import command.Command;
 
 public class PowerCommand extends Command {
 
-        public double calculateValue(List<Object> param) {
-                return Math.pow((double) param.get(0), (double) param.get(1));
-        }
+	public double calculateValue(List<Object> param) {
+		return Math.pow((double) param.get(0), (double) param.get(1));
+	}
 }
