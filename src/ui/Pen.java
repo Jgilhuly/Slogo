83e9537baee5cf25a33eb0c70ec3fac5b6b792b4
jgilhuly <jrg37@duke.edu>
2015-03-06 00:41:10 -0500

    Added line draw hierarchy which can draw different types of lines

--- src/ui/Pen.java
+++ src/ui/Pen.java
@@ -9,35 +9,50 @@ public class Pen {
 	private Canvas myCanvas;
 	private double canvasCenterX;
 	private double canvasCenterY;
-	
+	private LineDrawHelper artists[] = {new NormalLineDrawHelper(), new DashedLineDrawHelper(), new DottedLineDrawHelper()};
+	private LineDrawHelper myArtist;
+
 	public Pen(Canvas canvasIn, Color startingColor, boolean penIsDownIn) {
 		myCanvas = canvasIn;
 		myColor = startingColor;
 		penIsDown = penIsDownIn;
 		canvasCenterX = myCanvas.getWidth() / 2;
 		canvasCenterY = myCanvas.getHeight() / 2;
+		myArtist = artists[0];
 	}
-	
+
 	public void draw(double x1, double y1, double x2, double y2,
-			 boolean hasTurtle) {
+			boolean hasTurtle) {
 		myCanvas.getGraphicsContext2D().setStroke(myColor);
 		myCanvas.getGraphicsContext2D().setLineWidth(3);
 
 		// minus y since it's flipped in the canvas
-		if (penIsDown)
-			myCanvas.getGraphicsContext2D().strokeLine(x1, y1,
+		if (penIsDown) {
+			myArtist.drawLine(myCanvas, x1, y1,
 					canvasCenterX + x2, canvasCenterY - y2);
+		}
 	}
 
 	public void setPenIsDown(boolean penIsDownIn) {
 		penIsDown = penIsDownIn;
 	}
-	
+
 	public Color getColor() {
 		return myColor;
 	}
 
 	public void setColor(Color color) {
 		myColor = color;
+	}
+
+	public void setLineStyle (String ls) {
+		if (ls.equals("normal"))
+			myArtist = artists[0];
+		else if (ls.equals("dashed"))
+			myArtist = artists[1];
+		if (ls.equals("dotted"))
+			myArtist = artists[2];
+		
+		System.out.println(ls);
 	}
 }
--- src/ui/TPropertiesElement.java
+++ src/ui/TPropertiesElement.java
@@ -4,9 +4,12 @@ import java.util.ResourceBundle;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -17,11 +20,12 @@ public class TPropertiesElement {
 	private TurtleView tView;
 	private Pen myPen;
 
-	public TPropertiesElement(ResourceBundle resourcesIn, TurtleView tViewIn, Pen penIn) {
+	public TPropertiesElement(ResourceBundle resourcesIn, TurtleView tViewIn,
+			Pen penIn) {
 		tView = tViewIn;
 		myResources = resourcesIn;
 		myPen = penIn;
-		
+
 		init();
 	}
 
@@ -29,28 +33,49 @@ public class TPropertiesElement {
 		myBaseNode = new VBox();
 		myBaseNode.setSpacing(5);
 
-		ToggleButton penUpDown = new ToggleButton(myResources.getString("PenDownToggle"));
+		ToggleButton penUpDown = makeToggleButton(myResources.getString("PenDownToggle"), null, null);
 		penUpDown.setOnAction(e -> myPen.setPenIsDown(!penUpDown.isSelected()));
 
-		ToggleButton turtleVisible = new ToggleButton(myResources.getString("TurtleHideToggle"));
+		ToggleButton turtleVisible = makeToggleButton(myResources.getString("TurtleHideToggle"), null, null);
 		turtleVisible.setOnAction(e -> tView.setTurtleVisible(turtleVisible.isSelected()));
 
-		myBaseNode.getChildren().addAll(penUpDown, turtleVisible, makeTurtleHeadingBox());
+		ToggleGroup lineStyleGroup = new ToggleGroup();
+		ToggleButton normal = makeToggleButton(myResources.getString("NormalLine"), e -> myPen.setLineStyle("normal"), lineStyleGroup);
+		ToggleButton dashed = makeToggleButton(myResources.getString("DashedLine"), e -> myPen.setLineStyle("dashed"), lineStyleGroup);
+		ToggleButton dotted = makeToggleButton(myResources.getString("DottedLine"), e -> myPen.setLineStyle("dotted"), lineStyleGroup);
+
+		myBaseNode.getChildren().addAll(penUpDown, turtleVisible, makeTurtleHeadingBox(), normal, dashed, dotted);
+	}
+
+	private ToggleButton makeToggleButton(String property,
+			EventHandler<ActionEvent> handler, ToggleGroup myGroup) {
+		ToggleButton result = new ToggleButton();
+		result.setText(property);
+		if (handler != null) {			
+			result.setOnAction(handler);
+		}
+		if (myGroup != null) {
+			result.setToggleGroup(myGroup);			
+		}
+		return result;
 	}
 
 	private HBox makeTurtleHeadingBox() {
 		HBox turtleHeadingBox = new HBox();
 		turtleHeadingBox.setSpacing(5);
-		Label turtleHeadingLabel = new Label(myResources.getString("TurtleHeadingLabel"));
-		
+		Label turtleHeadingLabel = new Label(
+				myResources.getString("TurtleHeadingLabel"));
+
 		Label turtleHeadingValue = new Label("0.0");
 		tView.getHeading().addListener(new ChangeListener<Object>() {
-			public void changed(ObservableValue<?> o, Object oldVal, Object newVal) {
+			public void changed(ObservableValue<?> o, Object oldVal,
+					Object newVal) {
 				turtleHeadingValue.setText(newVal.toString());
 			}
 		});
-		
-		turtleHeadingBox.getChildren().addAll(turtleHeadingLabel, turtleHeadingValue);
+
+		turtleHeadingBox.getChildren().addAll(turtleHeadingLabel,
+				turtleHeadingValue);
 		return turtleHeadingBox;
 	}
 
