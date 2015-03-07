c03381222443bbaedea0008f57dce25b0a93b6ea
ycz2@duke.edu <ycz2@duke.edu>
2015-03-07 00:04:24 -0500

    Got rid of constructors in Comand superclass

--- src/command/Command.java
+++ src/command/Command.java
@@ -1,27 +1,7 @@
 package command;
 
-
-import java.util.List;
-
-
 public abstract class Command {
-    protected double double1;
-    protected double double2;
-    
-    public Command(){
-        //is this empty constructor necessary?
-    }
-    
-    public Command(double num){
-        double1 = num;
-    }
-    
-    public Command(double num1, double num2){
-        double1 = num1;
-        double2 = num2;
-    }
 
-//    public abstract double calculateValue(List<Object> param);
     public abstract double calculateValue();
 }
 
