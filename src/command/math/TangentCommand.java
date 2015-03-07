a8cddccde2ac5c2f0e3d08117f758757a0d85db7
gtnet8@gmail.com <gtnet8@gmail.com>
2015-03-07 01:25:02 -0500

    fix dependency with creating turtle

--- src/command/math/TangentCommand.java
+++ src/command/math/TangentCommand.java
@@ -3,10 +3,6 @@ package command.math;
 import command.Command;
 
 public class TangentCommand extends Command {
-<<<<<<< HEAD
-
-=======
->>>>>>> 0d4b48ce23f685cc2b6adbbd8dfb2fefd1fb5a4a
     private double double1;
     
     public TangentCommand(double op1){
@@ -16,8 +12,5 @@ public class TangentCommand extends Command {
     public double calculateValue(){
         return ((double) Math.tan(Math.toRadians(double1)));
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> 0d4b48ce23f685cc2b6adbbd8dfb2fefd1fb5a4a
 }
