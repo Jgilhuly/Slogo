ecc3e56e804d0171140aced3e7263b887d2a91b3
gtnet8@gmail.com <gtnet8@gmail.com>
2015-03-07 01:21:49 -0500

    fixing tangent, commited with error

--- src/command/math/TangentCommand.java
+++ src/command/math/TangentCommand.java
@@ -3,14 +3,7 @@ package command.math;
 import command.Command;
 
 public class TangentCommand extends Command {
-<<<<<<< HEAD
-	
-	public double calculateValue(double o1) {
-		return  Math.tan(Math.toRadians(o1));
-	}
 
-
-=======
     private double double1;
     
     public TangentCommand(double op1){
@@ -20,5 +13,5 @@ public class TangentCommand extends Command {
     public double calculateValue(){
         return ((double) Math.tan(Math.toRadians(double1)));
     }
->>>>>>> e2d604e384bd08560f3e3ec2461f93eb9b024847
+
 }
